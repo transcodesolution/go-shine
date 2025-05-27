baeb781ea62ab6307b2385dfff6b282dc85f5a30
dixita <transcode.dixita@gmail.com>
2025-05-27 18:47:43 +0530

    feat: implement login, forgot password, reset password, appointment, and thank you pages

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('passwordInput');
+    const togglePassword = document.getElementById('togglePassword');
+    const eyeIcon = document.getElementById('eyeIcon');
+    const eyeOffIcon = document.getElementById('eyeOffIcon');
+
+    if (togglePassword && passwordInput && eyeIcon && eyeOffIcon) {
+        togglePassword.addEventListener('click', function () {
+            const isPassword = passwordInput.type === 'password';
+            passwordInput.type = isPassword ? 'text' : 'password';
+            eyeIcon.style.display = isPassword ? 'block' : 'none';
+            eyeOffIcon.style.display = isPassword ? 'none' : 'block';
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabButtons = document.querySelectorAll('.tab-btn');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabButtons.forEach(btn => {
+        btn.addEventListener('click', function () {
+            tabButtons.forEach(b => {
+                b.classList.remove('bg-pink-50');
+                // b.classList.add('border-transparent', 'text-gray-600');
+            });
+            this.classList.add('bg-pink-50');
+
+            // Content
+            const tab = this.getAttribute('data-tab');
+            tabContents.forEach(content => {
+                content.classList.add('hidden');
+            });
+            document.getElementById('tab-' + tab).classList.remove('hidden');
+        });
+    });
+});
+
+//dropdown
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector("#dropdownButton");
+    const menu = document.getElementById("dropdownMenu");
+    const valueSpan = document.getElementById("dropdownValue");
+
+    button.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+
+    window.setValue = function (val) {
+        valueSpan.textContent = val;
+        menu.classList.add("hidden");
+    };
+
+    // Close dropdown when clicking outside
+    window.addEventListener("click", function (e) {
+        if (!button.contains(e.target)) {
+            menu.classList.add("hidden");
+        }
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4E273A', // Main  purple
+        pink: {
+          950: '#4E273A', // Primary accents, highlights
+          50: "#F6E4EB" // Light pink for backgrounds, borders
+        },
+        gray: {
+          600: '#737373', // placeholder text,
+          400: '#A0A0A0',
+          300: '#EDEDED', // Light gray for backgrounds, borders
+          200: "#C6C6C6", // Light gray for text disables
+        },
+        yellow: {
+          700: '#EB9C73', // Accent button 
+        },
+        black: {
+          900: '#190C12', // Strong body text
+        },
+        green: {
+          500: "#00852C" // Success messages, notifications
+        },
+        red: {
+          500: "#FF0004", // Error messages, notifications
+        }
+      },
+
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          lg: '1240px',
+          xl: '1240px',
+        },
+      },
+      fontsize: {
+        '4xl': '32px', // 32px
+      }
+    },
+    plugins: [],
+  }
+}
